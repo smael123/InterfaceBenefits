daac12233c57541887f0a95f92ef302be6364257
Ismael Almaguer <ismaelalmaguer@Ismaels-MacBook-Air.local>
2025-09-11 21:14:22 -0500

    made some changes that were in generic branch

--- InterfaceBenefits/Utils/VendorDuplicateFinder.cs
+++ InterfaceBenefits/Utils/VendorDuplicateFinder.cs
@@ -22,14 +22,17 @@ public static class VendorDuplicateFinder
             {
                 duplicatedData.Add(vendor.BusinessName);
             }
-            
         }
 
+        Console.WriteLine("Duplicated Vendors");
+        Console.WriteLine();
+        
         foreach (string key in duplicateDictionary.Keys)
         {
             HashSet<string> duplicateArray = duplicateDictionary[key];
-
-            Console.WriteLine($"Vendor with id: {key} is duplicated. Duplicated Data {string.Join(" | ", duplicateArray)}");
+            
+            if (duplicateArray.Count > 1)
+                Console.WriteLine($"Id: {key} is duplicated. Duplicated Data: {string.Join(" | ", duplicateArray)}");
         }
     }
 }
