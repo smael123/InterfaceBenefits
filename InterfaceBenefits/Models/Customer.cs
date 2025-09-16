3c5d441ab9545f16e3eeb658f9be874d8abc186a
Ismael Almaguer <ismaelalmaguer@Ismaels-MacBook-Air.local>
2025-09-15 22:38:45 -0500

    moved commit that had first part of solution to its own branch

--- InterfaceBenefits/Models/Customer.cs
+++ InterfaceBenefits/Models/Customer.cs
@@ -4,10 +4,20 @@ namespace InterfaceBenefits.Models;
 /// <summary>
 /// Receives financial aid from vendors.
 /// </summary>
-public class Customer
+public class Customer : IDuplicatableItem
 {
     public required string SocialSecurityNumber { get; set; }
     public required bool IsHomeless { get; set; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
+
+    public string GetDisplaybaleNonUniqueData()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
+    public string GetUniqueId()
+    {
+        return SocialSecurityNumber;
+    }
 }
--- InterfaceBenefits/Models/Vendor.cs
+++ InterfaceBenefits/Models/Vendor.cs
@@ -3,9 +3,19 @@ namespace InterfaceBenefits.Models;
 /// <summary>
 /// Distributes financial aid to customers
 /// </summary>
-public class Vendor
+public class Vendor : IDuplicatableItem
 {
     public required string TaxId { get; set; }
     public required string BusinessName { get; set; }
     public required bool IsCompliance { get; set; }
+
+    public string GetDisplaybaleNonUniqueData()
+    {
+        return BusinessName;
+    }
+
+    public string GetUniqueId()
+    {
+        return TaxId;
+    }
 }
